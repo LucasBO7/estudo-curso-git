18c22813d223cb6e8e37ab8bb1d1eb6226a8016f
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2021-07-06 16:07:42 -0300

    Acrescentado mensagem desejando um bom dia

--- Estudo Cap GIT GITHUB/Program.cs
+++ Estudo Cap GIT GITHUB/Program.cs
@@ -9,6 +9,7 @@ namespace Estudo_Cap_GIT_GITHUB
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
         }
     }
